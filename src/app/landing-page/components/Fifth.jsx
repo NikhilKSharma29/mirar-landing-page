79c490b9c9b0efb5e98e7aa35ec9de02f0093b1a
Nikhil sharma <sharmanikhil2188@gmail.com>
2025-08-27 23:19:24 +0530

    code ui fixed

--- src/app/landing-page/components/Fifth.jsx
+++ src/app/landing-page/components/Fifth.jsx
@@ -20,7 +20,7 @@ const Fifth = () => {
         {/* WhatsApp */}
         <div className="bg-orange-100 rounded-2xl shadow p-8 flex flex-col items-center hover:scale-105 transition">
             <FaWhatsapp size={58} className="text-blue-500 mb-4" />
-            <h3 className="font-bold text-2xl mb-2">WhatsApp</h3>
+            <h3 className="font-bold text-2xl mb-2 text-black">WhatsApp</h3>
             <p className="text-gray-700 text-sm text-black">
                 Accept orders, collect payments, and arrange delivery, all in one chat.
             </p>
@@ -29,7 +29,7 @@ const Fifth = () => {
         {/* Messenger */}
         <div className="bg-orange-100 rounded-2xl shadow p-8 flex flex-col items-center hover:scale-105 transition">
             <RiMessengerLine size={58} className="text-blue-500 mb-4" />
-            <h3 className="font-bold text-2xl mb-2">Messenger</h3>
+            <h3 className="font-bold text-2xl mb-2 text-black">Messenger</h3>
             <p className="text-gray-700 text-sm text-black">
                 Customers order, pay, and track deliveries without leaving Messenger —
                 all within Messenger itself.
@@ -39,7 +39,7 @@ const Fifth = () => {
         {/* Instagram */}
         <div className="bg-orange-100 rounded-2xl shadow p-8 flex flex-col items-center hover:scale-105 transition">
             <FaInstagram size={58} className="text-blue-500 mb-4" />
-            <h3 className="font-bold text-2xl mb-2">Instagram</h3>
+            <h3 className="font-bold text-2xl mb-2 text-black">Instagram</h3>
             <p className="text-gray-700 text-sm text-black">
                 Turn DMs into sales, from menu browsing to payments and delivery.
                 Mirar AI trains itself instantly.
